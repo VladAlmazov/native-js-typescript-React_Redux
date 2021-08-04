98689d05924ff581edfde55ae0053f511685e4b1
VladAlmazov <vladosa113@gmail.com>
2021-08-04 22:07:51 +0300

    From class to functional Component

---
+++ src/redux/actions.ts
@@ -0,0 +1,70 @@
+export enum ACTIONS_TYPE {
+    CHANGE_CURRENCY_FIELD_TYPE = 'CurrencyExchange/CHANGE_CURRENCY_FIELD_TYPE',
+    CHANGE_CHANGE_ACTION = 'CurrencyExchange/CHANGE_CHANGE_ACTION',
+    CHANGE_CURRENT_CURRENCY = 'CurrencyExchange/CHANGE_CURRENT_CURRENCY',
+}
+
+
+export type ChangeCurrencyFieldType = {
+    type: ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE
+    payload: {
+        amountOfBYN: string
+        amountOfCurrency: string
+    }
+};
+
+// @ts-ignore
+export const ChangeCurrencyFieldAC = (amountOfBYN: string, amountOfCurrency: string): ChangeCurrencyFieldType => {
+    return {
+        type: ACTIONS_TYPE.CHANGE_CURRENCY_FIELD_TYPE,
+        payload: {amountOfBYN, amountOfCurrency},
+    };
+};
+
+export type ChangeAction = {
+    type: ACTIONS_TYPE.CHANGE_CHANGE_ACTION
+    payload: {
+        isBuying: boolean,
+        amountOfBYN: '',
+        amountOfCurrency: '',
+    }
+};
+
+// @ts-ignore
+export const ChangeActionAC = (isBuying: boolean): ChangeAction => {
+    // amountOfBYN = '' - для правильных вычислений при смене валюты
+    // amountOfCurrency = '' - для правильных вычислений при смене валюты
+    return {
+        type: ACTIONS_TYPE.CHANGE_CHANGE_ACTION,
+        payload: {
+            isBuying,
+            amountOfBYN: '',
+            amountOfCurrency: '',
+        }
+    }
+};
+
+export type ChangeCurrentCurrencyType = {
+    type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY
+    payload: {
+        currentCurrency: string,
+        amountOfBYN: '',
+        amountOfCurrency: '',
+    }
+};
+
+// @ts-ignore
+export const ChangeCurrentCurrencyAC = (currentCurrency: string): ChangeCurrentCurrencyType => {
+    // amountOfBYN = '' - для правильных вычислений при смене валюты
+    // amountOfCurrency = '' - для правильных вычислений при смене валюты
+    return {
+        type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY,
+        payload: {
+            currentCurrency,
+            amountOfBYN: '',
+            amountOfCurrency: '',
+        },
+    }
+};
+
+export type CurrencyReducersTypes = ChangeCurrencyFieldType | ChangeAction | ChangeCurrentCurrencyType;
